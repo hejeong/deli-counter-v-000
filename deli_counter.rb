f1ea867c0089aeb648f76ddecef6544ff7bcbbf4
Jonathan Hong <jonhong777@gmail.com>
2018-07-03 16:44:07 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,8 +3,11 @@ def line(array)
   if array.length === 0
     puts "The line is currently empty."
   else
-    array.each do |person|
-      puts person
-    end
+    puts "The line is currently: "
   end
+end
+
+def take_a_number(line, name)
+  line.push(name)
+  puts "Welcome, #{name}. You are number #{line.length}"
 end
